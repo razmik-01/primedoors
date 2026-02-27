d94ead6c16da0860d264a6516987dfb15eb0fb7b
Razmik <razarakelyan01@gmail.com>
2026-02-28 00:07:53 +0400

    Add files via upload

--- primedoors_bot.py
+++ primedoors_bot.py
@@ -6,7 +6,7 @@ TOKEN = "7895178842:AAEKH25qO9xc0y5y1e0_ZmH0uVMr-x9-VaE"
 async def calculate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         x = float(update.message.text)
-        result = (x * 1.05) * 1.6
+        result = ((x * 1.05) + 4000) * 1.6
         await update.message.reply_text(f"Ամբողջական արժեք: {result}")
     except:
         await update.message.reply_text("Խնդրում եմ մուտագրել միայն թվեր")
